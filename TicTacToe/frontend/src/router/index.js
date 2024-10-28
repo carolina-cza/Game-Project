8c55ce3e0e0b8c5c9105233e1a311a7842356408
reco74 <152899081+reco74@users.noreply.github.com>
2024-10-28 20:10:46 +0100

    Add: joinID router link

--- TicTacToe/frontend/src/router/index.js
+++ TicTacToe/frontend/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TheWelcomeView from '@/views/TheWelcomeView.vue'
 import TicTacToeView from '@/views/TicTacToeView.vue'
+import JoinID from '@/views/JoinID.vue'
 
 const routes = [
   { path: '/', component: TheWelcomeView },
+  { path: '/joinid', component: JoinID },
   { path: '/tictactoe', component: TicTacToeView }
 ]
 
